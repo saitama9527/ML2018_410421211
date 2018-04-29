8a958f5a0ff3477f10bebd5aa69401b0a2163436
Saitama <taf10563@icloud.com>
2018-04-29 18:56:27 +0800

    Array test

--- encry.py
+++ encry.py
@@ -9,15 +9,39 @@ k1 = Image.open("key1.png")
 print(k1.format, k1.size ,k1.mode)
 k2 = Image.open("key2.png")
 print(k2.format, k2.size ,k2.mode)
+im = Image.open("I.png")
+print(im.format, im.size ,im.mode)
+e = Image.open("E.png")
+print(e.format, e.size ,e.mode)
 
 epa = np.asarray(ep).copy()
 k1a = np.asarray(k1).copy()
 k2a = np.asarray(k2).copy()
+ima = np.asarray(im).copy()
+ea = np.asarray(e).copy()
 
+i = 400*300
+w1 = w2 = w3 = 0.1
+
+w = np.array([w1,w1,w3])
+
+#x = np.array([k1a[0,0],k2a[0,0],ima[0,0]])
+#y = np.array([1,2,3])
+#xt = x.T
+
+#print(x*y)
 
 eptest = epa-k1a-k2a
 
-print(eptest)
+
+for k in range(0, 299):
+    for p in range(0, 399):
+        x[k*p] = np.array(k1a[k,p],k2a[k,p],ima[k,p])
+        a[k*p] =
+
+
+
+
 
 etest = Image.fromarray(eptest)
 etest.save("test.png")
